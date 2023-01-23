def92faa82f426971c5be40c8c7231284da1a80a
magnon <magnong000@gmail.com>
2023-01-23 13:06:15 +0100

    json to persistent objects complet

--- Part 4/json_loads.py
+++ Part 4/json_loads.py
@@ -1,14 +1,14 @@
 import json
 import shelve
-from tkinter import filedialog  # todo: keep window on top
+from tkinter import filedialog
 
 
-def ask_obj_path() -> str:  # todo: change to savename
-    return filedialog.askdirectory()
+def ask_obj_path() -> str:
+    return filedialog.asksaveasfilename(title=u'Save Python persistent object', filetypes=[])
 
 
 def ask_json_file() -> str:
-    return filedialog.askopenfilename(title=u'Load JSON file:', filetypes=[("JSON", ".json")])
+    return filedialog.askopenfilename(title=u'Load JSON file', filetypes=[("JSON", ".json")])
 
 
 def convert_json(file: str) -> dict:
@@ -49,23 +49,27 @@ def get_router_num_list(routers_list: list) -> list:
     # list of router numbers, ex.[1,14] for 'router1' 'router14' keys of a dict.
 
 
-def save_obj(obj_path: str, write_data: tuple, as_num_list, router_num_list) -> None:
+def save_obj(obj_name: str, write_data: tuple, as_num_list, router_num_list) -> None:
     """use 2 lists: 'routers', 'router-neighbors' to name the keys of dict"""
-    with shelve.open(obj_path + '/test') as p_obj:
+    with shelve.open(obj_name) as p_obj:
         key_list = ['as_', 'neighbor_as_', 'asbr_as_', 'routers_as_', 'neighbor_router_']
         num_list = [as_num_list, as_num_list, as_num_list, as_num_list, router_num_list]
         index = 0
         for lst in write_data:
             count = 0
-            for ele in lst:
-                p_obj[key_list[index] + str(num_list[index][count])] = ele
+            for elem in lst:
+                p_obj[key_list[index] + str(num_list[index][count])] = elem
                 count += 1
             index += 1
 
 
 if __name__ == '__main__':
     json_path = ask_json_file()
-    save_path = ask_obj_path()
+    save_path_empty = True  # avoid empty name
+    while save_path_empty:
+        save_path = ask_obj_path()
+        if save_path:
+            save_path_empty = False
     converted_dict = convert_json(json_path)
     tuple_5_lists = get_5_lists(converted_dict)
     as_nb_list = get_as_num_list(tuple_5_lists[0])
@@ -73,7 +77,7 @@ if __name__ == '__main__':
     router_nb_list = get_router_num_list(tuple_5_lists[3])
     print("router number list:", router_nb_list)
     save_obj(save_path, tuple_5_lists, as_nb_list, router_nb_list)
-    with shelve.open("../Part 2/test") as test:
+    with shelve.open(save_path) as test:
         for ele in test:
             print(ele)
         print(test['asbr_as_2'])
