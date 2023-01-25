0c3080de1911bdedd570ca2e5826009f3c278593
MONTEAGUDO Diego <d.monteagudo13@gmail.com>
2023-01-25 15:26:01 +0100

    tests avec réparations

--- Part 4/main.py
+++ Part 4/main.py
@@ -24,35 +24,27 @@ routeur1 = Router(1, astest1)
 routeur2 = ASBR(2, astest1)
 routeur3 = ASBR(3, astest2)
 
-interface1 = Interface("0", "1::/16", routeur1, routeur2)
+interface1 = Interface(0, "1::/16", routeur1, routeur2)
 routeur1.add_existing_interfaces(interface1)
 
-print("add_interface_from_neighbor_router au routeur2 en ajoutant le routeur 1:")
 routeur2.add_interface_from_neighbor_router("0", routeur1)
 
-print("add_interface_from_neighbor_router au routeur3 en ajoutant le routeur 2:")
-routeur3.add_interface_from_neighbor_router("0", routeur2)
+routeur3.add_interface_from_neighbor_router("0", routeur2, "12::/16")
 
-print("add_interface_from_neighbor_router au routeur2 en ajoutant le routeur 3:")
-routeur2.add_interface_from_neighbor_router("1", routeur3)
+routeur2.add_interface_from_neighbor_router("1", routeur3, "12::/16")
 
-print("add_loopback_interface au routeur 2:")
 routeur2.add_loopback_interface()
-
-print(astest1.routers[0])
 
 astest1.description()
 astest2.description()
 
 routeur1.interfaces[0].craft_ip()
-routeur2.interfaces[0].craft_ip()
 routeur3.interfaces[0].craft_ip()
-routeur2.interfaces[1].craft_ip()
+routeur2.craft_ip_on_all_interfaces()
 
 print(astest1.AS_neighbors)
 
 print(routeur2.interfaces)
 print(routeur2.get_sorted_list_of_interfaces())
 
-print(type(routeur1))
 
